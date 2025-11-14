91932e79d2b666d8a85308072f402ac65ef0ee86
Pablo Masquiarán <pablo.masquiaran@gmail.com>
2025-11-14 19:09:22 -0300

    Creación del componente ARViewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
-import React, { useEffect } from "react";
-import "aframe";
+import ARViewer from "./componentes/ARViewer.jsx";
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    // Carga dinámica del script de AR.js
-    const script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/gh/AR-js-org/AR.js@3.4.7/aframe/build/aframe-ar.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <a-scene
-        vr-mode-ui="enabled: false"
-        embedded
-        arjs="sourceType: webcam; debugUIEnabled: false;"
-      >
-        {/* Marcador tipo "hiro" (puedes imprimirlo o mostrarlo en otro dispositivo) */}
-        <a-marker preset="hiro">
-          <a-box position="0 0.5 0" material="color: red;"></a-box>
-        </a-marker>
-
-        <a-entity camera></a-entity>
-      </a-scene>
-    </div>
-  );
+  return <ARViewer />;
 }
 
 export default App
---
+++ src/componentes/ARViewer.jsx
@@ -0,0 +1,26 @@
+function ARViewer() {
+  return (
+    <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
+        renderer="logarithmicDepthBuffer: true;"
+      >
+        {/* MARCADOR TIPO HIRO */}
+        <a-marker preset="hiro">
+          <a-box
+            position="0 0.5 0"
+            material="color: red;"
+            animation="property: rotation; to: 0 360 0; loop: true; dur: 4000"
+          ></a-box>
+        </a-marker>
+
+        {/* CÁMARA */}
+        <a-entity camera></a-entity>
+      </a-scene>
+    </div>
+  );
+}
+
+export default ARViewer
